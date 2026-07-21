47dd76f02e672f399ba6dbff06dface7a1d2250a
depthsky2016 <19619104+depthsky2016@users.noreply.github.com>
2026-07-21 11:33:43 +0800

    shotbox 0.8.6

--- Casks/shotbox.rb
+++ Casks/shotbox.rb
@@ -1,6 +1,6 @@
 cask "shotbox" do
-  version "0.8.5"
-  sha256 "fc5061a12fc77f124742634a9c7e4589ed686566e6b368d63ad071027e830d3c"
+  version "0.8.6"
+  sha256 "c5054b38498df872334561ffb992cdf37798fe9f254f2fa6dfa921cbdd2eacb1"
 
   url "https://dl-box.gatheon.com/Shotbox-#{version}.dmg"
   name "Shotbox"
